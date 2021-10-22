8bbd30993933eed222060f7e191e5eb4c573f247
Anathapindika Muliawan <anathapindika.muliawan@ui.ac.id>
2021-10-23 01:12:28 +0700

    hamburger, responsive homepage

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,32 +1,48 @@
 import React from "react";
+import { Turn as Hamburger } from "hamburger-react";
 import { Link } from "react-scroll";
 import { Link as LinkRouter } from "react-router-dom";
+import { useState } from "react";
 import "./Header.css";
 
 const Header = () => {
+    const [isOpen, setOpen] = useState(false);
+
     function scrollWin() {
         window.scrollTo(0, 0);
     }
 
     return (
         <div className="header">
-            <ul>
-                <li className="header-list">
-                    <LinkRouter className="header-link underline-transition" to="/" onClick={scrollWin}>
-                        Home{" "}
-                    </LinkRouter>
-                </li>
-                <li className="header-list">
-                    <Link className="header-link underline-transition" to="project">
-                        Project{" "}
-                    </Link>
-                </li>
-                <li className="header-list">
-                    <a className="header-link underline-transition" href="https://medium.com/@anathapindika.m" target="_blank" rel="noopener noreferrer">
-                        Blog
-                    </a>
-                </li>
-            </ul>
+            <div className="hamburger">
+                <Hamburger toggled={isOpen} toggle={setOpen} size={30} className="hamburger-icon" />
+            </div>
+            <div className={isOpen ? "" : "hidden"}>
+                <ul>
+                    <li className="header-list">
+                        <LinkRouter
+                            className="header-link underline-transition"
+                            to="/"
+                            onClick={() => {
+                                setOpen(false);
+                                scrollWin();
+                            }}
+                        >
+                            Home{" "}
+                        </LinkRouter>
+                    </li>
+                    <li className="header-list">
+                        <Link className="header-link underline-transition" to="project" onClick={() => setOpen(false)}>
+                            Project{" "}
+                        </Link>
+                    </li>
+                    <li className="header-list">
+                        <a className="header-link underline-transition" href="https://medium.com/@anathapindika.m" target="_blank" rel="noopener noreferrer" onClick={() => setOpen(false)}>
+                            Blog
+                        </a>
+                    </li>
+                </ul>
+            </div>
         </div>
     );
 };
--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { useState, useEffect } from "react";
 import { Link } from "react-scroll";
 import Foto from "../assets/foto.png";
 import { AiOutlineInstagram, AiOutlineLinkedin, AiOutlineYoutube, AiOutlineGithub, AiOutlineMail, AiOutlineTwitter } from "react-icons/ai";
@@ -11,6 +12,26 @@ const HomePage = () => {
     function scrollWin() {
         window.scrollTo(0, 0);
     }
+
+    const [isDesktop, setDesktop] = useState(window.innerWidth > 900);
+
+    const updateMedia = () => {
+        setDesktop(window.innerWidth > 900);
+    };
+
+    useEffect(() => {
+        window.addEventListener("resize", updateMedia);
+        return () => window.removeEventListener("resize", updateMedia);
+    });
+
+    const aboutmeText =
+        "My name is Anathapindika Muliawan. I'm a college student and a Wushu athlete for 12 years. I'm interested in Programming and Web Development. I love to make small projects to improve my skills. I work pretty well as a team. I know C/C++ Programming language and Python. I use React.Js and CSS to make websites.";
+
+    const [isReadMore, setIsReadMore] = useState(true);
+
+    const toggleReadMore = () => {
+        setIsReadMore(!isReadMore);
+    };
 
     return (
         <div className="homepage" id="homepage">
@@ -47,8 +68,12 @@ const HomePage = () => {
                     <div className="aboutme-container-text">
                         <div className="aboutme-text">
                             <p>
-                                My name is Anathapindika Muliawan. I'm a college student and a Wushu athlete for 12 years. I'm interested in Programming and Web Development. I love to make small projects to improve my skills. I work pretty
-                                well as a team. I know C/C++ Programming language and Python. I use React.Js and CSS to make websites.
+                                {/* {isDesktop ? <div>aaa</div> : [isReadMore ? aboutmeText.slice(0, 150) : <p>{aboutmeText}</p>]}
+
+                                {isDesktop ? [isReadMore ? aboutmeText.slice(0, 150) : <p>{aboutmeText}</p>
+                                <span onClick={toggleReadMore} className="read-or-hide">
+                                    {isReadMore ? "...read more" : " show less"}
+                                </span>] : <div>aaa</div>} */}
                             </p>
                         </div>
                         <div>
